7df1e604ef63a1cf3b5efc7a4864cd41e7e0c48b
Igor Correa <igoracorrea@gmail.com>
2021-12-26 16:34:10 -0400

    Update out information. Change icon.

--- app/src/main/java/br/com/imc/modelo/Calculo.java
+++ app/src/main/java/br/com/imc/modelo/Calculo.java
@@ -1,33 +1,41 @@
 package br.com.imc.modelo;
 
-
-
 public class Calculo {
 
 
-    private final double saida;
+    private final double imc;
+    private final double pesoMax;
+    private final double pesoMin;
+
 
     public Calculo(double altura, double peso){
-        String valor = String.format("%.1f", peso / Math.pow(altura, 2));
-        valor = valor.replace(',', '.');
-        this.saida = Double.parseDouble(valor);
+        double alturaQuadrado = Math.pow(altura,2);
+        this.pesoMin = formatar(18.5 * alturaQuadrado) - 0.1;
+        this.pesoMax = formatar(24.9 * alturaQuadrado) + 0.1;
+        this.imc = formatar(peso / alturaQuadrado);
     }
 
     public String getValor(){
-        if(this.saida < 18.5){
-            return "IMC = "+this.saida+" Você está Abaixo do Peso";
-        }if(this.saida >= 18.5 && this.saida <= 24.9){
-            return "IMC = "+this.saida+" Você está com o peso Normal";
-        }if(this.saida > 24.9 && this.saida <= 29.9){
-            return "IMC = "+this.saida+" Você está com Sobrepeso";
-        }if(this.saida > 29.9 && this.saida <= 34.9){
-            return "IMC = "+this.saida+" Você está em Obesidade Grau 1";
-        }if(this.saida > 34.9 && this.saida <= 39.9){
-            return "IMC = "+this.saida+" Você está em Obesidade Grau 2";
+        if(this.imc < 18.5){
+            return "IMC = "+this.imc +" Você está Abaixo do Peso.\nSeu peso ideal é " + pesoMin + " até " + pesoMax;
+        }if(this.imc >= 18.5 && this.imc <= 24.9){
+            return "IMC = "+this.imc +" Você está com o peso Normal.\nSeu peso ideal é " + pesoMin + " até " + pesoMax;
+        }if(this.imc > 24.9 && this.imc <= 29.9){
+            return "IMC = "+this.imc +" Você está com Sobrepeso.\nSeu peso ideal é " + pesoMin + " até " + pesoMax;
+        }if(this.imc > 29.9 && this.imc <= 34.9){
+            return "IMC = "+this.imc +" Você está em Obesidade Grau 1.\nSeu peso ideal é " + pesoMin + " até " + pesoMax;
+        }if(this.imc > 34.9 && this.imc <= 39.9){
+            return "IMC = "+this.imc +" Você está em Obesidade Grau 2.\nSeu peso ideal é " + pesoMin + " até " + pesoMax;
         }else{
-            return "IMC = "+this.saida+" Você está em Obesidade Mórbida";
+            return "IMC = "+this.imc +" Você está em Obesidade Mórbida.\nSeu peso ideal é " + pesoMin + " até " + pesoMax;
         }
 
+    }
+
+    private double formatar(double resultado){
+        String valor = String.format("%.1f", resultado);
+        valor = valor.replace(',', '.');
+        return Double.parseDouble(valor);
     }
 
 }
